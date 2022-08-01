c56fad334a5f9e0d48deec16690b6d6f5efe5f89
mdhossin <mdsahadathossin778@gmail.com>
2022-08-01 10:37:07 +0600

    added animation using aos

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Header, Hero } from "./components";
+// import aos
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 function App() {
   // mobile nav state
   const [navMobile, setNavMobile] = useState(false);
+
+  // aos init
+  useEffect(() => {
+    Aos.init({
+      duration: 2500,
+      delay: 400,
+    });
+  });
   return (
     <div className="overflow-hidden">
       <Header setNavMobile={setNavMobile} />
--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -11,7 +11,11 @@ const Hero = () => {
 
           <div className="flex-1">
             {/* badge text */}
-            <div className="bg-white/10 p-1 mb-6 rounded-full pl-1 pr-3 max-w-[365px]">
+            <div
+              className="bg-white/10 p-1 mb-6 rounded-full pl-1 pr-3 max-w-[365px]"
+              data-aos="fade-down"
+              data-aos-delay="400"
+            >
               <div className="flex items-center justify-between text-sm lg:text-base">
                 <div className="bg-white text-darkblue rounded-full font-medium py-1 px-4">
                   75% SAVE
@@ -21,22 +25,34 @@ const Hero = () => {
             </div>
 
             {/* title */}
-            <h1 className="text-[32px] lg:text-[64px] font-bold leading-tight mb-6">
+            <h1
+              className="text-[32px] lg:text-[64px] font-bold leading-tight mb-6"
+              data-aos="fade-down"
+              data-aos-delay="500"
+            >
               Fastest & secure platform to invest in crypto.
             </h1>
 
-            <p className="max-w-[440px] leading-relaxed mb-8">
+            <p
+              className="max-w-[440px] leading-relaxed mb-8"
+              data-aos="fade-down"
+              data-aos-delay="600"
+            >
               Buy and sell cryptocurrencies, trusted by 10M wallets with over
               $30 billion in transactions.
             </p>
 
-            <button className="btn gap-x-6 pl-6 text-sm lg:h-16 lg:text-base">
+            <button
+              className="btn gap-x-6 pl-6 text-sm lg:h-16 lg:text-base"
+              data-aos="fade-down"
+              data-aos-delay="700"
+            >
               Try for free
               <IoIosArrowDroprightCircle className="text-2xl lg:text-3xl" />
             </button>
           </div>
           {/* hero image */}
-          <div className="flex-1">
+          <div className="flex-1" data-aos="fade-up" data-aos-delay="600">
             <img src={HeroImg} alt="alt" />
           </div>
         </div>
